dc6265b6fd10b18c2b948b9d859f23539815db9d
reha <reha.gursel@aegeanlabs.com>
2025-05-12 06:45:42 +0300

    fix: build

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://ai-chat-ui-eight.vercel.app/"
+    "https://ai-chat-ui-eight.vercel.app"
 ];
 
 const corsOptions = {
